3b5ca7d831046063fa1dfbbb5434056338507be4
류재준 <ryungom@gmail.com>
2024-11-06 15:28:39 +0900

    :bulb: add: day04 수강 작업 내역 업로드

--- practice_project/section02/src/components/searchable-layout.tsx
+++ practice_project/section02/src/components/searchable-layout.tsx
@@ -1,7 +1,40 @@
-export default function SearchableLayout({ children }: { children: React.ReactNode }) {
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+
+export default function SearchableLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const route = useRouter();
+  const [search, setSearch] = useState("");
+  const q = route.query.q as string;
+  useEffect(() => {
+    setSearch(q || "");
+  }, [q]);
+  const inputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (!search) return;
+    setSearch(e.target.value);
+  };
+  const pageMove = () => {
+    route.push(`/search?q=${search}`);
+  };
+  const keyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if ((e.key = "enter")) {
+      pageMove();
+    }
+  };
+
   return (
     <div>
-      <div>임시 서치바</div>
+      <input
+        type="text"
+        value={search}
+        onChange={inputChange}
+        onKeyDown={keyDown43}
+        placeholder="검색어를 입력해주세요 ..."
+      />
+      <button onClick={pageMove}>검색</button>
       {children}
     </div>
   );
--- practice_project/section02/src/pages/search/index.tsx
+++ practice_project/section02/src/pages/search/index.tsx
@@ -1,3 +1,14 @@
+import SearchableLayout from "@/components/searchable-layout";
+import { useRouter } from "next/router";
+import { ReactNode } from "react";
+
 export default function Page() {
-  return <h1>서취2</h1>;
+  const route = useRouter();
+  const { q } = route.query;
+
+  return <h1>서취값 : {q}</h1>;
 }
+
+Page.getLayout = (page: ReactNode) => {
+  return <SearchableLayout>{page}</SearchableLayout>;
+};
