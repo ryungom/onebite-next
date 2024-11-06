e0b2a25e45b35bf03415176d4782b2ebe4ec0436
류재준 <ryungom@gmail.com>
2024-11-06 15:57:40 +0900

    💡add: day04 수강 작업 내용 업로드

--- practice_project/section02/src/components/searchable-layout.tsx
+++ practice_project/section02/src/components/searchable-layout.tsx
@@ -1,40 +1,37 @@
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import style from "./searchable-layout.module.css";
 
-export default function SearchableLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function SearchableLayout({ children }: { children: React.ReactNode }) {
   const route = useRouter();
   const [search, setSearch] = useState("");
   const q = route.query.q as string;
+
   useEffect(() => {
     setSearch(q || "");
   }, [q]);
+
   const inputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (!search) return;
     setSearch(e.target.value);
   };
+
   const pageMove = () => {
+    if (!search || q === search) return;
     route.push(`/search?q=${search}`);
   };
+
   const keyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if ((e.key = "enter")) {
+    if (e.key === "Enter") {
       pageMove();
     }
   };
 
   return (
     <div>
-      <input
-        type="text"
-        value={search}
-        onChange={inputChange}
-        onKeyDown={keyDown43}
-        placeholder="검색어를 입력해주세요 ..."
-      />
-      <button onClick={pageMove}>검색</button>
+      <div className={style.searchbar_container}>
+        <input type="text" value={search} onChange={inputChange} onKeyDown={keyDown} placeholder="검색어를 입력해주세요 ..." />
+        <button onClick={pageMove}>검색</button>
+      </div>
       {children}
     </div>
   );
