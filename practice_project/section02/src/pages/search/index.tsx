70b9c6958c799a5b88ee309d710fdf5863cabbcb
류재준 <ryungom@gmail.com>
2024-11-06 20:57:53 +0900

    💡add: day05 수강 작업 내용 업로드

--- practice_project/section02/src/pages/search/index.tsx
+++ practice_project/section02/src/pages/search/index.tsx
@@ -1,12 +1,17 @@
 import SearchableLayout from "@/components/searchable-layout";
 import { useRouter } from "next/router";
 import { ReactNode } from "react";
+import books from "@/mock/books.json";
+import BookItem from "@/components/book-item";
 
 export default function Page() {
-  const route = useRouter();
-  const { q } = route.query;
-
-  return <h1>서취값 : {q}</h1>;
+  return (
+    <div>
+      {books.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </div>
+  );
 }
 
 Page.getLayout = (page: ReactNode) => {
