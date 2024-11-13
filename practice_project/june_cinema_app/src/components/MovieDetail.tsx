78513ffcb2fb6f09864e31cd0bf9f8bedbb9f186
류재준 <ryungom@gmail.com>
2024-11-14 00:48:06 +0900

    💡add: day12 챌린지 업로드

--- practice_project/june_cinema_app/src/components/MovieDetail.tsx
+++ practice_project/june_cinema_app/src/components/MovieDetail.tsx
@@ -3,8 +3,6 @@ import Link from "next/link";
 import style from "./MovieDetail.module.css";
 
 export default function MovieDetail({ id, posterImgUrl }: MovieData) {
-  console.log("들어왔나요?", id);
-  console.log("들어왔나요?", posterImgUrl);
   return (
     <>
       <Link href={`/movie/${id}`} className={style.container}>
