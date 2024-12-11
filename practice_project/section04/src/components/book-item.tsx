4b38cb2d8d461271efcf938753ae9a34a011159e
류재준 <ryungom@gmail.com>
2024-12-11 22:08:53 +0900

    :bulb: day19 수강내용 업로드

--- practice_project/section04/src/components/book-item.tsx
+++ practice_project/section04/src/components/book-item.tsx
@@ -1,6 +1,7 @@
 import type { BookData } from "@/types";
 import Link from "next/link";
 import style from "./book-item.module.css";
+import Image from "next/image";
 
 export default function BookItem({
   id,
@@ -13,7 +14,14 @@ export default function BookItem({
 }: BookData) {
   return (
     <Link href={`/book/${id}`} className={style.container}>
-      <img src={coverImgUrl} />
+      <figure>
+        <Image
+          src={coverImgUrl}
+          fill
+          sizes="100%"
+          alt={"도서이미지 자료입니다"}
+        />
+      </figure>
       <div>
         <div className={style.title}>{title}</div>
         <div className={style.subTitle}>{subTitle}</div>
